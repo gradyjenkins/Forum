5a11c0bb09075635d13bc89cc590943686526303
GRADY <gjenkins205@gmail.com>
2017-11-30 21:53:26 -0500

    Initial Commit

---
+++ Forum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forum
+//
+//  Created by Grady Jenkins on 11/30/17.
+//  Copyright © 2017 Grady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
